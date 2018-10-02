1fa555e3073e504e7b5038a75a3ae13de96f4022
christophpickl <christoph.pickl@gmail.com>
2018-10-02 22:43:00 +0200

    abstract factory done

---
+++ src/main/kotlin/com/github/christophpickl/designpatterns4k/creational/abstract_factory.kt
@@ -0,0 +1,41 @@
+package com.github.christophpickl.designpatterns4k.creational
+
+interface Database
+class DevDatabase : Database
+class ProdDatabase : Database
+
+interface Network
+class DevNetwork : Network
+class ProdNetwork : Network
+
+// alias the "abstract factory"
+interface Environment {
+    fun database(): Database
+    fun network(): Network
+}
+class DevEnvironment : Environment {
+    override fun database() = DevDatabase()
+    override fun network() = DevNetwork()
+}
+class ProdEnvironment : Environment {
+    override fun database() = ProdDatabase()
+    override fun network() = ProdNetwork()
+}
+object CurrentEnvironment {
+    val environment: Environment = DevEnvironment()
+//    val environment: Environment = ProdEnvironment()
+}
+
+class Service(private val database: Database) {
+    fun act() {
+        println("Using database: ${database.javaClass.name}")
+    }
+}
+
+object AbstractFactoryApp {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val service = Service(CurrentEnvironment.environment.database())
+        service.act()
+    }
+}
